a3279e6416192d410e34fb02642673f17797a21c
cgreenup <charlirejunk@gmail.com>
2020-02-14 12:18:15 -0600

    Changes to log messages

--- app/src/main/java/com/chrisgreenup/tictactoevariants/MainActivity.java
+++ app/src/main/java/com/chrisgreenup/tictactoevariants/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void setupMain(){
+        Log.i("TTTMenu", "In activity_main now");
         setContentView(R.layout.activity_main);
         inMainActivity = true;
 
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void setupSelect(){
+        Log.i("TTTMenu", "In activity_select now");
         setContentView(R.layout.activity_select);
         inMainActivity = false;
 
@@ -54,15 +56,15 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View view) {
             if(view.getId() == R.id.game_one_button){
                 //Game one button code here
-                Log.i("Button", "Button = 1");
+                Log.i("TTTButton", "Button = 1");
             }
             else if(view.getId() == R.id.game_two_button){
                 //Game two button code here
-                Log.i("Button", "Button = 2");
+                Log.i("TTTButton", "Button = 2");
             }
             else if (view.getId() == R.id.game_three_button){
                 //Game Three button code here
-                Log.i("Button", "Button = 3");
+                Log.i("TTTButton", "Button = 3");
             }
             else{
                 //Otherwise, the return button has been pressed
