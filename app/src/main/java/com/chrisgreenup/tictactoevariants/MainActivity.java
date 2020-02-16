dab1c75885c32857b6a4b513f39f919b609ef44b
cgreenup <charlirejunk@gmail.com>
2020-02-15 23:59:51 -0600

    - Created activity_board.xml and have a rough layout for it. Not yet completed - Created the setup for activity_board in the main java code - Created the listener for the board itself. Probably should use a different listener for the clear button, and any other buttons that may or may not be created

--- app/src/main/java/com/chrisgreenup/tictactoevariants/MainActivity.java
+++ app/src/main/java/com/chrisgreenup/tictactoevariants/MainActivity.java
@@ -7,9 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private boolean inMainActivity;
+
+    private ArrayList<Character> [][] board;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Sets up the UI to show activity_main.xml
+    // Hooks up the button to go to the select activity
+    // Hooks up the button which gives info to the user
     void setupMain(){
         Log.i("TTTMenu", "In activity_main now");
         setContentView(R.layout.activity_main);
@@ -62,6 +68,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    void setupBoard(){
+        setContentView(R.layout.activity_board);
+
+        initializeTheBoardButtons();
+        //TODO: implement method of writing board state to a file
+        //TODO: implement a method of reading file and applying to board state
+
+        //TODO: implement saving the board (writing) when leaving board in any form
+        //TODO: implement board clearing
+    }
+
+    //  Hooks up all of the buttons on activity_board.xml
+    //  This is its own method to improve readability when setting up the board because this
+    //  is just a mess, and makes the other method hard to read
+    void initializeTheBoardButtons(){
+        findViewById(R.id.space_0_0).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_0_1).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_0_2).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_1_0).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_1_1).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_1_2).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_2_0).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_2_1).setOnClickListener(new BoardButton());
+        findViewById(R.id.space_2_2).setOnClickListener(new BoardButton());
+        findViewById(R.id.reset_board_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Put the board clearing thing here
+            }
+        });
+    }
+
     //Handles all of the onClick actions for the buttons in activity_main.xml
     class MenuButton implements OnClickListener{
         @Override
@@ -82,19 +120,29 @@ public class MainActivity extends AppCompatActivity {
             if(view.getId() == R.id.game_one_button){
                 //Game one button code here
                 Log.i("TTTButton", "Button = 1");
+                setupBoard();
             }
             else if(view.getId() == R.id.game_two_button){
                 //Game two button code here
                 Log.i("TTTButton", "Button = 2");
+                setupBoard();
             }
             else if (view.getId() == R.id.game_three_button){
                 //Game Three button code here
                 Log.i("TTTButton", "Button = 3");
+                setupBoard();
             }
             else{
                 //Otherwise, the return button has been pressed
                 onBackPressed();
             }
+        }
+    }
+
+    class BoardButton implements  OnClickListener{
+        @Override
+        public void onClick(View view) {
+            //TODO: do the game stuff here
         }
     }
 
