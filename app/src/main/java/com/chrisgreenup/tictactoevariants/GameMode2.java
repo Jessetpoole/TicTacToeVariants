23269412a55a0b19a5b0eb99f20a4b786e2a0677
Jessetpoole <Jessetpoole@gmail.com>
2020-02-16 18:45:59 -0600

    Setting up gamemode2 java

--- app/src/main/java/com/chrisgreenup/tictactoevariants/GameMode2.java
+++ app/src/main/java/com/chrisgreenup/tictactoevariants/GameMode2.java
@@ -65,5 +65,51 @@ public class GameMode2 extends AppCompatActivity implements View.OnClickListener
         }
         roundCounts++;
 
+
+
+    }
+
+    private boolean checkForWin(){
+
+        String[][] board=new String[3][3];
+
+        for (int i=0; i<3; i++)
+            for (int t=0; t <3; t++)
+                board[i][t]=buttons[i][t].getText().toString();
+
+
+        for(int i =0; i<3; i++) {
+            if (board[i][0].equals(board[i][1]) && board[i][0].equals(board[i][2])
+                    && !board[i][0].equals("")) {
+
+                return true;
+            }
+
+        }
+
+
+        for(int i =0; i<3; i++) {
+            if (board[0][i].equals(board[1][i]) && board[0][i].equals(board[2][i])
+                    && !board[0][i].equals("")) {
+
+                return true;
+            }
+
+        }
+
+
+        if(board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2])
+                && !board[0][0].equals("")) {
+            return true;
+        }
+
+        if(board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0])
+                && !board[0][2].equals("")) {
+            return true;
+        }
+
+
+        return false;
+
     }
 }
